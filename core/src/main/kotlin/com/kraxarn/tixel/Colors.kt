e865e59a79ac79baabff2e351eb8125c5fa5a3bb
kraxarn <kraxie@protonmail.com>
2022-05-22 11:45:23 +0200

    Colors constants

---
+++ core/src/main/kotlin/com/kraxarn/tixel/Colors.kt
@@ -0,0 +1,12 @@
+package com.kraxarn.tixel
+
+import com.badlogic.gdx.graphics.Color
+
+class Colors()
+{
+	companion object
+	{
+		val background = Color(0x33_99_da_ff)
+		val foreground = Color(Color.WHITE)
+	}
+}
