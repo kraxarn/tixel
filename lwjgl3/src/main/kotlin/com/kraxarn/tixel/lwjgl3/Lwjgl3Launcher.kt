84c25b4cdc006b3e690389d8c3e7c6d1bfba7a67
kraxarn <kraxie@protonmail.com>
2022-05-26 13:04:08 +0200

    Basic crash handler

--- lwjgl3/src/main/kotlin/com/kraxarn/tixel/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/com/kraxarn/tixel/lwjgl3/Lwjgl3Launcher.kt
@@ -5,12 +5,27 @@ package com.kraxarn.tixel.lwjgl3
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.kraxarn.tixel.Game
+import javax.swing.JOptionPane
+import kotlin.system.exitProcess
 
 fun main()
 {
-	Lwjgl3Application(Game(), Lwjgl3ApplicationConfiguration().apply {
-		setTitle("Tixel")
-		setWindowedMode(1280, 720)
-		setWindowIcon("image/icon.png")
-	})
+	try
+	{
+		Lwjgl3Application(Game(), Lwjgl3ApplicationConfiguration().apply {
+			setTitle("Tixel")
+			setWindowedMode(1280, 720)
+			setWindowIcon("image/icon.png")
+		})
+	}
+	catch (e: Exception)
+	{
+		e.printStackTrace()
+
+		JOptionPane.showMessageDialog(
+			null, e.stackTraceToString(),
+			"Fatal error", JOptionPane.ERROR_MESSAGE
+		)
+		exitProcess(e.hashCode())
+	}
 }
