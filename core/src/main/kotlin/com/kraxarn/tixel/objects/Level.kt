73eba7cc77e437715687845b6e352dd2c12cbf5e
kraxarn <kraxie@protonmail.com>
2022-05-27 16:09:36 +0200

    Spawn and gem count

--- core/src/main/kotlin/com/kraxarn/tixel/objects/Level.kt
+++ core/src/main/kotlin/com/kraxarn/tixel/objects/Level.kt
@@ -1,7 +1,10 @@
 package com.kraxarn.tixel.objects
 
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.Json
 import com.badlogic.gdx.utils.JsonValue
+import com.kraxarn.tixel.enums.Tile
+import com.kraxarn.tixel.extensions.times
 import ktx.json.readArrayValue
 import ktx.json.readValue
 
@@ -12,6 +15,9 @@ class Level(
 	var map: List<List<Int>> = emptyList(),
 ) : Json.Serializable
 {
+	private val gemCount: Int = getGemCount()
+	private val spawn: Vector2 = getSpawn() ?: Vector2.Zero
+
 	override fun read(json: Json, jsonData: JsonValue)
 	{
 		name = json.readValue(jsonData, "name")
@@ -26,4 +32,29 @@ class Level(
 		json.writeValue("tileset", tileset)
 		json.writeValue("music", music)
 	}
+
+	private fun getGemCount(): Int = map.flatten().count { it == Tile.GEM.id }
+
+	private fun getSpawn(): Vector2?
+	{
+		for (y in 0..map.count())
+		{
+			for (x in 0..map[y].count())
+			{
+				if (map[y][x] == Tile.SPAWN.id)
+				{
+					return Vector2(x.toFloat(), y.toFloat())
+				}
+			}
+		}
+		return null
+	}
+
+	val safeSpawn: Vector2
+		get()
+		{
+			val playerPosition = spawn * Tiles.tileSize
+			playerPosition.y -= Tiles.tileSize * 0.5f
+			return playerPosition
+		}
 }
