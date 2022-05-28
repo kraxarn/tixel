21a87f7fa96f5265c469fb6d7c50988f5b63af1e
kraxarn <kraxie@protonmail.com>
2022-05-28 12:11:49 +0200

    All available tiles in enum

--- core/src/main/kotlin/com/kraxarn/tixel/enums/Tile.kt
+++ core/src/main/kotlin/com/kraxarn/tixel/enums/Tile.kt
@@ -1,14 +1,27 @@
 package com.kraxarn.tixel.enums
 
-enum class Tile(val id: Int)
+enum class Tile(val id: Int, val type: TileType)
 {
-	NONE(-1),
-	SPAWN(50),
-	EXIT(51),
-	COIN(52),
-	GEM(53),
-	WATER(54),
-	LAVA(55),
-	SPIKE(56),
-	BOSS(57),
+	// Invalid
+	NONE(-1, TileType.EMPTY),
+
+	// From level atlas
+	TILE_RIGHT(0, TileType.TILE),
+	TILE_LEFT(1, TileType.TILE),
+	TILE_MIDDLE(2, TileType.TILE),
+	TILE_CENTER(3, TileType.TILE),
+	ONE_WAY_RIGHT(4, TileType.ONE_WAY),
+	ONE_WAY_SINGLE(5, TileType.ONE_WAY),
+	ONE_WAY_CENTER(6, TileType.ONE_WAY),
+	ONE_WAY_LEFT(7, TileType.ONE_WAY),
+
+	// From item atlas
+	SPAWN(50, TileType.ITEM),
+	EXIT(51, TileType.ITEM),
+	COIN(52, TileType.ITEM),
+	GEM(53, TileType.ITEM),
+	WATER(54, TileType.ITEM),
+	LAVA(55, TileType.ITEM),
+	SPIKE(56, TileType.ITEM),
+	BOSS(57, TileType.ENTITY),
 }
