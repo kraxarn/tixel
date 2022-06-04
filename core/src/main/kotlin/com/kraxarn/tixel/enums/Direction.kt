415a70d36f62518e1e171e7c0577980e0d9013ae
kraxarn <kraxie@protonmail.com>
2022-06-04 14:57:00 +0200

    Direction flags

--- core/src/main/kotlin/com/kraxarn/tixel/enums/Direction.kt
+++ core/src/main/kotlin/com/kraxarn/tixel/enums/Direction.kt
@@ -1,10 +1,10 @@
 package com.kraxarn.tixel.enums
 
-enum class Direction
+enum class Direction(val flag: Int)
 {
-	NONE,
-	LEFT,
-	UP,
-	RIGHT,
-	DOWN,
+	NONE(0),
+	LEFT(1),
+	UP(2),
+	RIGHT(4),
+	DOWN(8),
 }
