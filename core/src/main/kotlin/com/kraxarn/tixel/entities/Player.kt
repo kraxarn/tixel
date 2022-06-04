b5484b02b25101bd09a455de84ff28432f4365b8
kraxarn <kraxie@protonmail.com>
2022-06-04 11:16:22 +0200

    Initial player update logic

--- core/src/main/kotlin/com/kraxarn/tixel/entities/Player.kt
+++ core/src/main/kotlin/com/kraxarn/tixel/entities/Player.kt
@@ -3,23 +3,114 @@ package com.kraxarn.tixel.entities
 import com.badlogic.gdx.math.Vector2
 import com.kraxarn.tixel.enums.AtlasAnimation
 import com.kraxarn.tixel.enums.Direction
+import com.kraxarn.tixel.enums.Key
+import com.kraxarn.tixel.extensions.isPressed
+import com.kraxarn.tixel.extensions.plus
+import com.kraxarn.tixel.extensions.y
+import com.kraxarn.tixel.objects.Level
+import com.kraxarn.tixel.physics.Collision
+import kotlin.math.max
+import kotlin.math.min
 
-class Player(hud: Hud) : AnimatedSprite(AtlasAnimation.PLAYER_RUNNING)
+class Player(private val hud: Hud) : AnimatedSprite(AtlasAnimation.PLAYER_RUNNING)
 {
 	private var velocity = Vector2()
 
 	val speed get() = velocity
 
 	// FIXME: Velocity Y is also 0 when hitting ceilings
-	val isGrounded get() = velocity.y == 0f
+	private val isGrounded get() = velocity.y == 0f
 
-	val direction
+	private val direction
 		get() = when
 		{
 			velocity.x < 0 -> Direction.LEFT
 			velocity.x > 0 -> Direction.RIGHT
 			else -> verticalDirection
 		}
+
+	fun update(level: Level, isPaused: Boolean)
+	{
+		if (!isPaused)
+		{
+			val speedLimitX = SPEED_LIMIT * hud.playerSpeedModifier.mod
+
+			// Right
+			if (Key.RIGHT.isPressed)
+			{
+				velocity.x = min(velocity.x + MOVE_ACCELERATION, speedLimitX)
+			}
+			else if (velocity.x > 0 && isGrounded)
+			{
+				velocity.x = max(velocity.x + MOVE_DECELERATION, 0f)
+			}
+
+			// Left
+			if (Key.LEFT.isPressed)
+			{
+				velocity.x = max(velocity.x - MOVE_ACCELERATION, -speedLimitX)
+			}
+			else if (velocity.x < 0 && isGrounded)
+			{
+				velocity.x = min(velocity.x - MOVE_DECELERATION, 0f)
+			}
+
+			// Jump
+			if (Key.JUMP.isPressed && isGrounded)
+			{
+				// TODO: Play jump sound
+				super.isPaused = true
+				frameIndex = 1
+				velocity.y = JUMP_FORCE
+			}
+			else
+			{
+				velocity.y += GRAVITY
+			}
+		}
+
+		// Check if sprite should be flipped
+		flipX = direction == Direction.RIGHT
+
+		// Update position
+		if (!isPaused)
+		{
+			if (!hud.isDead)
+			{
+				updateCollision(level)
+			}
+
+			position += velocity
+
+			if (velocity.x == 0f)
+			{
+				super.isPaused = true
+				frameIndex = 0
+			}
+			else
+			{
+				super.isPaused = false
+			}
+		}
+
+		// Die
+		if (y > KILL_PLAIN)
+		{
+			if (!hud.isDead)
+			{
+				// TODO: Play fall sound
+			}
+
+			velocity = Vector2()
+			position = level.safeSpawn
+			hud.respawn()
+		}
+	}
+
+	private fun updateCollision(level: Level)
+	{
+		Collision.update(rect, level, velocity, hud)
+	}
 
 	companion object
 	{
