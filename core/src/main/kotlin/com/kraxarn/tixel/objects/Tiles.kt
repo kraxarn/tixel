07529453304361e74d134e50107649397972a870
kraxarn <kraxie@protonmail.com>
2022-06-04 11:36:00 +0200

    Make private

--- core/src/main/kotlin/com/kraxarn/tixel/objects/Tiles.kt
+++ core/src/main/kotlin/com/kraxarn/tixel/objects/Tiles.kt
@@ -5,12 +5,12 @@ object Tiles
 	/**
 	 * Size of each tile in image asset
 	 */
-	const val TILESET_SIZE = 18;
+	private const val TILESET_SIZE = 18;
 
 	/**
 	 * How much to scale each tile before displaying
 	 */
-	const val TILE_SCALE = 3f;
+	private const val TILE_SCALE = 3f;
 
 	/**
 	 * Total tile size with scaling applied
